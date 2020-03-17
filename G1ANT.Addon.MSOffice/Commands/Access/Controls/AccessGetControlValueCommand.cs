bc2eb775036ff7ff75f13cf4f8ca0cb2f453c8a1
Łukasz Fronczyk <lukasz.fronczyk@g1ant.com>
2020-03-17 17:09:06 +0100

    AB#984 setvalue -> setcontrolvalue, added getcontrolvalue

--- G1ANT.Addon.MSOffice/Commands/Access/Controls/AccessGetControlValueCommand.cs
+++ G1ANT.Addon.MSOffice/Commands/Access/Controls/AccessGetControlValueCommand.cs
@@ -9,29 +9,32 @@
 */
 
 using G1ANT.Language;
+using Newtonsoft.Json.Linq;
 
 namespace G1ANT.Addon.MSOffice.Commands.Access.Controls
 {
-    [Command(Name = "access.setcontrolvalue", Tooltip = "Set value of unbound control of form of Access application")]
-    public class AccessSetControlValueCommand : Command
+    [Command(Name = "access.getcontrolvalue", Tooltip = "Get value of control of form of Access application")]
+    public class AccessGetControlValueCommand : Command
     {
         public class Arguments : CommandArguments
         {
             [Argument(Required = true, Tooltip = "Path to the control. See `access.getcontrol` tooltip for path examples")]
             public TextStructure Path { get; set; }
 
-            [Argument(Required = true, Tooltip = "Value to set")]
-            public TextStructure Value { get; set; }
+            [Argument(Tooltip = "Name of a variable where the command's result will be stored")]
+            public VariableStructure Result { get; set; } = new VariableStructure("result");
         }
 
-        public AccessSetControlValueCommand(AbstractScripter scripter) : base(scripter)
+        public AccessGetControlValueCommand(AbstractScripter scripter) : base(scripter)
         { }
 
         public void Execute(Arguments arguments)
         {
             var control = AccessManager.CurrentAccess.GetControlByPath(arguments.Path.Value);
+            var value = control.GetValue();
+            var result = value is string ? (Structure)new TextStructure(value) : new JsonStructure(JObject.FromObject(value));
 
-            control.SetValue(arguments.Value.Value);
+            Scripter.Variables.SetVariableValue(arguments.Result.Value, result);
         }
     }
 }
