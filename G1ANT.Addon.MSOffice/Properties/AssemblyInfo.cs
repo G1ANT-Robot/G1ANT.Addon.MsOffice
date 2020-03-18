fee6860f3c92371447558fd2dd28bd37acb4c1d6
Łukasz Fronczyk <lukasz.fronczyk@g1ant.com>
2020-03-18 09:56:13 +0100

    updated AssemblyVersion, AssemblyFileVersion and AssemblyCopyright, fixed AssemblyTitle and AssemblyProduct

--- G1ANT.Addon.MSOffice/Properties/AssemblyInfo.cs
+++ G1ANT.Addon.MSOffice/Properties/AssemblyInfo.cs
@@ -14,12 +14,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("G1ANT.Language.Excel")]
+[assembly: AssemblyTitle("G1ANT.Addon.MSOffice")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("G1ANT.Language.Excel")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("G1ANT")]
+[assembly: AssemblyProduct("G1ANT.Addon.MSOffice")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -41,5 +41,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.205.17304.1000")]
-[assembly: AssemblyFileVersion("2.205.17304.1000")]
+[assembly: AssemblyVersion("3.0.*")]
+[assembly: AssemblyFileVersion("3.0.0")]
