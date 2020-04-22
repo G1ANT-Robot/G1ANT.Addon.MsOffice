c75e3336a05c05cf7d7071f18b2a05eb6dd2b737
Łukasz Fronczyk <lukasz.fronczyk@g1ant.com>
2020-04-22 23:04:59 +0200

    Access commands, panels and forms moved to separate repository (https://github.com/G1ANT-Robot/G1ANT.Addon.Access)

--- G1ANT.Addon.MSOffice/Models/Access/IDetailedNameModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace G1ANT.Addon.MSOffice.Models.Access
-{
-    internal interface IDetailedNameModel
-    {
-        string ToDetailedString();
-    }
-}
--- G1ANT.Addon.MSOffice/Models/Access/INameModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace G1ANT.Addon.MSOffice.Models.Access
-{
-    internal interface INameModel
-    {
-        string Name { get; }
-    }
-}
