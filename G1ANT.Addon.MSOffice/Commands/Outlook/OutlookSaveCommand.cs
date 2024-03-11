522cb3cb378692169f2848359998e4277bdbbd9d
Przemek Witusik <pwitusik77@gmail.com>
2024-03-11 12:00:45 +0100

    add outlook.save command

---
+++ G1ANT.Addon.MSOffice/Commands/Outlook/OutlookSaveCommand.cs
@@ -0,0 +1,38 @@
+﻿using G1ANT.Language;
+using Microsoft.Office.Interop.Excel;
+using mshtml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G1ANT.Addon.MSOffice.Commands.Outlook
+{
+    [Command(Name = "outlook.save", Tooltip = "This command saves an email to specified file")]
+    public class OutlookSaveCommand : Command
+    {
+        public class Arguments : CommandArguments
+        {
+            [Argument(Tooltip = "Path to the file where message will be stored")]
+            public TextStructure Path { get; set; } 
+
+            [Argument(Tooltip = "Name of an `outlookmail` variable where a mail to be sent is stored")]
+            public OutlookMailStructure Mail { get; set; }
+        }
+        public OutlookSaveCommand(AbstractScripter scripter) : base(scripter)
+        {
+        }
+
+        public void Execute(Arguments arguments)
+        {
+            var outlookManager = OutlookManager.CurrentOutlook;
+            if (outlookManager != null)
+            {
+                outlookManager.SaveAs(arguments.Mail?.Value, arguments.Path?.Value);
+            }
+            else
+                throw new NullReferenceException("Current Outlook is not set.");
+        }
+    }
+}
